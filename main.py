cb0c49a018e2b1d29c79ff043e2aa5ba21fbeb8d
sevinm <79662332+sevinm@users.noreply.github.com>
2022-07-25 22:59:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # importing libraries
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize, sent_tokenize
